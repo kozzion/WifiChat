47ded7b2e6f304b150acd72b44b2cbea73b05eeb
Jaap Oosterbroek <jaap.oosterbroek@gmail.com>
2017-05-01 22:30:14 +0200

    boem werkt een beetje voor 2 telefoons

--- app/src/main/java/nl/everlutions/wifichat/MainActivity.java
+++ app/src/main/java/nl/everlutions/wifichat/MainActivity.java
@@ -1,13 +1,124 @@
 package nl.everlutions.wifichat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.nsd.NsdServiceInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.status)
+    TextView mStatusView;
+    NsdHelper mNsdHelper;
+
+    private Handler mUpdateHandler;
+
+    public static final String TAG = "NsdChat";
+
+    ChatConnection mConnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mUpdateHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                String chatLine = msg.getData().getString("msg");
+                addChatLine(chatLine);
+            }
+        };
+
+        mConnection = new ChatConnection(mUpdateHandler);
+
+        mNsdHelper = new NsdHelper(this);
+        mNsdHelper.initializeNsd();
+
+    }
+
+    public void clickAdvertise(View v) {
+        // Register service
+        if (mConnection.getLocalPort() > -1) {
+            mNsdHelper.registerService(mConnection.getLocalPort());
+        } else {
+            Log.d(TAG, "ServerSocket isn't bound.");
+        }
+    }
+
+    @OnClick(R.id.discover_btn)
+    public void clickDiscover(View v) {
+        Log.e(TAG, "joehoe");
+        mNsdHelper.discoverServices();
+
+    }
+
+
+    public void clickConnect(View v) {
+        NsdServiceInfo service = mNsdHelper.getChosenServiceInfo();
+        if (service != null) {
+            Log.d(TAG, "Connecting.");
+            mConnection.connectToServer(service.getHost(),
+                    service.getPort());
+        } else {
+            Log.d(TAG, "No service to connect to!");
+        }
+    }
+
+    public void clickSend(View v) {
+        EditText messageView = (EditText) this.findViewById(R.id.chatInput);
+        if (messageView != null) {
+            final String messageString = messageView.getText().toString();
+            if (!messageString.isEmpty()) {
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mConnection.sendMessage(messageString);
+                    }
+                }).start();
+
+
+            }
+            messageView.setText("");
+        }
+    }
+
+    public void addChatLine(String line) {
+        mStatusView.append("\n" + line);
+    }
+
+    @Override
+    protected void onPause() {
+        if (mNsdHelper != null) {
+            mNsdHelper.stopDiscovery();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mNsdHelper != null) {
+            mNsdHelper.discoverServices();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        mNsdHelper.tearDown();
+        mConnection.tearDown();
+        super.onDestroy();
     }
 }
